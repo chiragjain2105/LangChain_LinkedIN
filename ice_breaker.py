d1ffdf7f8cdebbab5361bcf28bb592e3e4446fb0
Abhishek-menia <abhishek.menia@incture.com>
2023-09-13 15:29:47 +0530

    parse_commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,20 +3,24 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from third_parties.linkedIn import scrape_linkedin_profile
 from Agents.linkedin_lookup_agents import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
+from typing import Tuple
 
-
-if __name__ == "__main__":
-    print("Hello LangChain!")
-
-    linkedin_profile_url = linkedin_lookup_agent(name="Chirag Jain NITJ'23")
+def ice_break(name: str) -> Tuple[PersonIntel,str]:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
     summary_template = """
         given the LinkedIN information {information} about a person I want you to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -29,5 +33,12 @@ if __name__ == "__main__":
 
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-    print(chain.run(information=linkedin_data))
+    result = chain.run(information=linkedin_data)
+    print(result)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
     # print(linkedin_data.json())
+
+
+if __name__ == "__main__":
+    print("Hello LangChain!")
+    result = ice_break(name="Chirag Jain NITJ'23")
